0984c9702b067c48ab24166581a70e14e57b986b
Ricky Payares <rpayaresswe@gmail.com>
2024-10-26 21:42:41 -0400

    Updates

---
+++ precache-manifest.984988dee7c38e0e26ea5b529f9108cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a0ff1b5b038f6080622295f6594c47",
+    "url": "/gallery-app/index.html"
+  },
+  {
+    "revision": "2466cf3be5b89c8ffc92",
+    "url": "/gallery-app/static/css/main.7becb7f2.chunk.css"
+  },
+  {
+    "revision": "5267f27e0b4cbe35663c",
+    "url": "/gallery-app/static/js/2.e0bc86cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery-app/static/js/2.e0bc86cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2466cf3be5b89c8ffc92",
+    "url": "/gallery-app/static/js/main.e248379d.chunk.js"
+  },
+  {
+    "revision": "ee48e0f0bb086498d37f",
+    "url": "/gallery-app/static/js/runtime-main.6ac7ee8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gallery-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
